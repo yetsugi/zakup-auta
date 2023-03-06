d58fbd029a4ba130b0c3d04e9a55edea63738bee
Yetsugi <yetsugi@gmail.com>
2023-03-06 15:04:26 +0100

    removed component

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-import { cars, brandFilterOptions } from "./car.js";
+import { cars } from "./car.js";
 
 const $brandFilter = document.querySelector("#brand-filter");
 const $carList = document.querySelector("#car-list");
+
+const brandFilterOptions = [
+  "Wszystkie",
+  ...new Set(cars.map((car) => car.brand)),
+];
 
 function fillBrandFilter() {
   brandFilterOptions.forEach((option) =>
@@ -18,14 +23,18 @@ function fillCarList(carArr) {
     const $carListItem = document.createElement("li");
 
     $carListItem.innerHTML = `
-      <car-li-content
-        car-img="${car.img}"
-        car-name="${car.nameStr}"
-        car-year="${car.year}"
-        car-engine-power="${car.enginePowerStr}"
-        car-mileage="${car.mileageStr}"
-        car-price="${car.priceStr}"
-      ></car-li-content>
+      <img src="${car.img}" alt="${car.nameStr} photo." />
+      <h2>${car.nameStr}</h2>
+      <dl>
+        <dt>Rocznik:</dt>
+        <dd>${car.year}</dd>
+        <dt>Moc:</dt>
+        <dd>${car.enginePowerStr}</dd>
+        <dt>Przebieg:</dt>
+        <dd>${car.mileageStr}</dd>
+        <dt>Cena:</dt>
+        <dd>${car.priceStr}</dd>
+      </dl>
     `;
 
     $carList.appendChild($carListItem);
@@ -33,12 +42,14 @@ function fillCarList(carArr) {
 }
 
 $brandFilter.addEventListener("change", (event) => {
-  const currentlySelected = event.target.value;
+  const selectedIndex = event.target.selectedIndex;
 
-  const carArr =
-    currentlySelected === "Wszystkie"
-      ? cars
-      : cars.filter((car) => car.brand === currentlySelected);
+  let carArr = cars;
+  if (selectedIndex !== 0) {
+    carArr = carArr.filter(
+      (car) => car.brand === brandFilterOptions[selectedIndex]
+    );
+  }
 
   fillCarList(carArr);
 });
